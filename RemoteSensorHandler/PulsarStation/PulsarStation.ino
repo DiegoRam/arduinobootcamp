c0e866ff7896f7e88ca7e12a4851ea7132db545c
diegoram <ddarkheart@gmail.com>
2014-04-02 15:20:16 -0300

    add sensor state transmission and fix arduino format message on Pulsar station

--- RemoteSensorHandler/PulsarStation/PulsarStation.ino
+++ RemoteSensorHandler/PulsarStation/PulsarStation.ino
@@ -5,7 +5,7 @@ RCSwitch mySwitch = RCSwitch();
 const int arduinoId = 10;
 const int greenLed = 4;
 const int pinTransmiter = 3;
-int sensorState = 1 ;
+int sensorState = 0 ;
 void setup(){
 	Serial.begin(9600);
 	mySwitch.enableTransmit(pinTransmiter);
@@ -18,7 +18,11 @@ void loop(){
 	digitalWrite(greenLed, HIGH);        
 	mySwitch.send(arduinoId + sensorState, 12);
 	digitalWrite(greenLed, LOW);
-        sensorState = sensorState * (-1);
+    if(sensorState==0){
+    	sensorState = 1;
+    } else {
+    	sensorState = 0;
+    }
     
 	Serial.print("broadcast message..");
         Serial.println(arduinoId + sensorState);
--- RemoteSensorHandler/SensorStateStation/SensorStateStation.ino
+++ RemoteSensorHandler/SensorStateStation/SensorStateStation.ino
@@ -1,27 +1,97 @@
+//Constants
+
 #include <RCSwitch.h>
+
+const int pinTrigger = 4;
+const int pinEcho = 3; // The echo pin must to have PWM support
+const int nosePin = 5;
+const int pinChannel1 = 7;
+const int takeTime = 5;
+const int timeUnit = 1000;
+const int pintTransmitter = 9;
+const int redLed = 6;
+int sensorState = 0;
+
+const int arduinoId = 20;
+
+
+boolean state = false;
+
+//Threshold to activate in cms
+const int perimeter = 90;
 
 RCSwitch mySwitch = RCSwitch();
 
-const int arduinoId = 10;
-const int greenLed = 4;
-const int pinTransmiter = 3;
-int sensorState = 1 ;
-void setup(){
-	Serial.begin(9600);
-	mySwitch.enableTransmit(pinTransmiter);
-	pinMode(greenLed, OUTPUT);
+long microsecondsToCm(long microseconds){
+  return microseconds / 29 / 2 ;
+}
+
+void turnOn()
+{
+	digitalWrite(nosePin, HIGH);
+	digitalWrite(pinChannel1, LOW);
+}
+
+void turnOff()
+{
+	digitalWrite(nosePin, LOW);
+	digitalWrite(pinChannel1, HIGH);
+}
+
+void sendStatus(){
+	Serial.print("sending sensor status: ");
+	Serial.println(sensorState);
+	mySwitch.send(arduinoId + sensorState ,12);
+        //mySwitch.sendTriState("from 2");
+	digitalWrite(redLed, HIGH);
 
 }
 
-void loop(){
+void setup()
+{
+	Serial.begin(9600);
+	pinMode(pinTrigger, OUTPUT);
+	pinMode(nosePin, OUTPUT);
+	pinMode(pinChannel1, OUTPUT);
+    pinMode(redLed, OUTPUT);
+    digitalWrite(nosePin, LOW);
+    digitalWrite(pinChannel1, HIGH);
 
-	digitalWrite(greenLed, HIGH);        
-	mySwitch.send(arduinoId + sensorState, 12);
-	digitalWrite(greenLed, LOW);
-        sensorState = sensorState * (-1);
-    
-	Serial.print("broadcast message..");
-        Serial.println(arduinoId + sensorState);
-	delay(2000);
-	
+    mySwitch.enableTransmit(pintTransmitter);
+}
+
+void loop()
+{
+	long duration , cm;
+	digitalWrite(redLed, LOW);
+
+  	pinMode(pinTrigger, OUTPUT);
+  	digitalWrite(pinTrigger, LOW);
+  	delayMicroseconds(2);
+  	digitalWrite(pinTrigger,HIGH);
+  	delayMicroseconds(10);
+  	digitalWrite(pinTrigger, LOW);
+
+	pinMode(pinEcho, INPUT);
+  	duration = pulseIn(pinEcho, HIGH);
+  	cm = microsecondsToCm(duration);
+
+  	Serial.println(cm);
+
+  	//Desicion
+        
+  	if(cm >= perimeter)
+  	{
+  	  Serial.print("turning off at ");
+      Serial.print(takeTime);
+      Serial.println(" seconds");
+  		turnOff();
+  		sensorState = 0 ; 
+  	} else {
+  		Serial.println("turning on...");
+  		turnOn();
+  		sensorState = 1 ;
+  	}
+  	sendStatus();
+    delay(timeUnit);
 }
